1956313a362f998328c9ba969fdc714fa9d7ac4a
zsubah <zsubah@ualberta.ca>
2024-10-30 20:53:23 -0600

    initialized main procedures

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEMORY_MAX (1 << 16) // 65,536 memory locations; maximum that is addressed by a 16-bit unsigned integer (2^16)
+#define MEMORY_MAX (1 << 16) // 65536 memory locations; maximum that is addressed by a 16-bit unsigned integer (2^16)
 
 __UINT16_TYPE__ memory[MEMORY_MAX]; // 65536 locations
 
@@ -53,3 +53,76 @@ enum
     FL_NEG = 1 << 2, /* N */
 };
 
+int main(int argc, const char* argv[])
+{
+    //@{Load Arguments}
+    //@{Setup}
+
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
+
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                //@{ADD}
+                break;
+            case OP_AND:
+                //@{AND}
+                break;
+            case OP_NOT:
+                //@{NOT}
+                break;
+            case OP_BR:
+                //@{BR}
+                break;
+            case OP_JMP:
+                //@{JMP}
+                break;
+            case OP_JSR:
+                //@{JSR}
+                break;
+            case OP_LD:
+                //@{LD}
+                break;
+            case OP_LDI:
+                //@{LDI}
+                break;
+            case OP_LDR:
+                //@{LDR}
+                break;
+            case OP_LEA:
+                //@{LEA}
+                break;
+            case OP_ST:
+                //@{ST}
+                break;
+            case OP_STI:
+                //@{STI}
+                break;
+            case OP_STR:
+                //@{STR}
+                break;
+            case OP_TRAP:
+                //@{TRAP}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                //@{BAD OPCODE}
+                break;
+        }
+    }
+    //@{Shutdown}
+}
